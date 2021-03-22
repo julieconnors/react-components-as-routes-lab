dc7112eb7b943753b20d74e280de6ac786f7c413
julieconnors <juliana.g.connors@gmail.com>
2021-03-22 15:50:40 -0400

    commit tree 9f8bbed6a3e98052d859ebbbb08967d6084f4bec

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import { directors } from '../data';
+import { actors, directors } from '../data';
 
 const Directors = () => {
+  console.log(directors)
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directors.map(director => {
+        return (
+          <div>
+            <h3>{director.name}</h3>
+            <p>Movies:</p>
+            {director.movies.map(movie => <li>{movie}</li>)}
+          </div>
+        )
+      })}
     </div>
   );
 }
