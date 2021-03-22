316e737d81eebab06bb426f526631bdff2ad09d3
julieconnors <juliana.g.connors@gmail.com>
2021-03-22 15:50:48 -0400

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,9 +2,18 @@ import React from 'react';
 import { actors } from '../data';
 
 const Actors = () => {
+  console.log(actors)
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map(actor => {
+        return (
+          <div className="actor" key={actor.name}>
+            <h2>Name: {`${actor.name}`}</h2>
+            <p>Movies:</p>
+            {actor.movies.map(movie => <li>{movie}</li>)}
+          </div>
+        )})}
     </div>
   );
 };
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,20 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+  console.log(movies)
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+        {movies.map(movie => {
+          return (
+            <div>
+              <h3>{movie.title}</h3>
+              <p>Time: {movie.time}</p>
+              <p>Genres:</p>
+              {movie.genres.map(genre => <li>{genre}</li>)}
+            </div>
+          )
+        })}
     </div>
   );
 };
